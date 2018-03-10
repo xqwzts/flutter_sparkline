498e122d36e0a552261ac91e6f6bcdd136e447f4
Victor Choueiri <victor@ctrlanddev.com>
2018-03-11 01:25:24 +0200

    Add fallbackWidth and fallbackHeight

--- lib/flutter_sparkline.dart
+++ lib/flutter_sparkline.dart
@@ -15,28 +15,52 @@ List<double> _generateRandomData(int count) {
   return result;
 }
 
+/// A widget that draws a sparkline chart.
+///
+/// By default, the sparkline is sized to fit its container. If the
+/// sparkline is in an unbounded space, it will size itself according to the
+/// given [fallbackWidth] and [fallbackHeight].
 class Sparkline extends StatelessWidget {
   Sparkline({
-    this.lineWidth = 5.0,
+    Key key,
+    this.lineWidth = 2.0,
     this.lineColor = Colors.lightBlue,
     this.sharpCorners = false,
-    this.fillColor = Colors.deepPurple,
-  });
+    this.fillColor = const Color(0xFF81D4FA), //Colors.lightBlue[200]
+    this.fallbackHeight = 100.0,
+    this.fallbackWidth = 300.0,
+  }) : super(key: key);
 
   final double lineWidth;
   final Color lineColor;
   final bool sharpCorners;
   final Color fillColor;
 
-  final List<double> data = _generateRandomData(15);
+  /// The width to use when the sparkline is in a situation with an unbounded
+  /// width.
+  ///
+  /// See also:
+  ///
+  ///  * [fallbackHeight], the same but vertically.
+  final double fallbackWidth;
+
+  /// The height to use when the sparkline is in a situation with an unbounded
+  /// height.
+  ///
+  /// See also:
+  ///
+  ///  * [fallbackWidth], the same but horizontally.
+  final double fallbackHeight;
+
+  final List<double> data = _generateRandomData(25);
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      color: Colors.red,
-      width: 200.0,
-      height: 100.0,
+    return new LimitedBox(
+      maxWidth: fallbackWidth,
+      maxHeight: fallbackHeight,
       child: new CustomPaint(
+        size: Size.infinite,
         painter: new _SparklinePainter(
           data,
           lineWidth: lineWidth,
@@ -103,9 +127,9 @@ class _SparklinePainter extends CustomPainter {
       fillPath.lineTo(0.0, size.height);
       fillPath.close();
       Paint fillPaint = new Paint()
-      ..strokeWidth = 0.0
-      ..color = fillColor
-      ..style = PaintingStyle.fill;
+        ..strokeWidth = 0.0
+        ..color = fillColor
+        ..style = PaintingStyle.fill;
       canvas.drawPath(fillPath, fillPaint);
     }
 
