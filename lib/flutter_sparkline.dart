a972df39dc897cf3f86a7fb23c03bb719dc66975
Victor Choueiri <victor@ctrlanddev.com>
2018-03-13 06:25:01 +0200

    Add pointSize and pointColor properties

--- lib/flutter_sparkline.dart
+++ lib/flutter_sparkline.dart
@@ -1,6 +1,7 @@
 library flutter_sparkline;
 
 import 'dart:math' as math;
+import 'dart:ui' as ui show PointMode;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -10,12 +11,19 @@ import 'package:flutter/material.dart';
 /// By default, the sparkline is sized to fit its container. If the
 /// sparkline is in an unbounded space, it will size itself according to the
 /// given [fallbackWidth] and [fallbackHeight].
+/// 
+/// If neither [pointSize] nor [pointColor] are specified then individual data
+/// points will not be drawn over the sparkline. If only one of them is provided
+/// then the other property will default to the corresponding line-value:
+/// [lineWidth] for the size and [lineColor] for the color.
 class Sparkline extends StatelessWidget {
   Sparkline({
     Key key,
     @required this.data,
     this.lineWidth = 2.0,
     this.lineColor = Colors.lightBlue,
+    this.pointSize,
+    this.pointColor,
     this.sharpCorners = false,
     this.fillColor = const Color(0xFF81D4FA), //Colors.lightBlue[200]
     this.fallbackHeight = 100.0,
@@ -26,6 +34,25 @@ class Sparkline extends StatelessWidget {
   final List<double> data;
   final double lineWidth;
   final Color lineColor;
+
+  /// The size to use when drawing individual data points over the sparkline.
+  /// 
+  /// At least one of [pointSize] or [pointColor] must be set, or data points
+  /// will not be drawn.
+  /// 
+  /// If [pointSize] is set, but [pointColor] is not, then [lineColor] will be
+  /// used as the color.
+  final double pointSize;
+
+  /// The color used when drawing individual data points over the sparkline.
+  /// 
+  /// At least one of [pointSize] or [pointColor] must be set, or data points
+  /// will not be drawn.
+  /// 
+  /// If [pointColor] is set, but [pointSize] is not, then [lineWidth] will be
+  /// used as the size.
+  final Color pointColor;
+
   final bool sharpCorners;
   final Color fillColor;
 
@@ -58,6 +85,8 @@ class Sparkline extends StatelessWidget {
           lineColor: lineColor,
           sharpCorners: sharpCorners,
           fillColor: fillColor,
+          pointSize: pointSize,
+          pointColor: pointColor,
         ),
       ),
     );
@@ -71,33 +100,40 @@ class _SparklinePainter extends CustomPainter {
     @required this.lineColor,
     @required this.sharpCorners,
     @required this.fillColor,
+    this.pointSize,
+    this.pointColor,
   })  : _max = dataPoints.reduce(math.max),
-        _min = dataPoints.reduce(math.min);
+        _min = dataPoints.reduce(math.min),
+        _drawPoints = pointSize != null || pointColor != null;
 
   final List<double> dataPoints;
   final double lineWidth;
-  final bool sharpCorners;
   final Color lineColor;
+  final bool sharpCorners;
   final Color fillColor;
+  final double pointSize;
+  final Color pointColor;
+  final bool _drawPoints;
 
   final double _max;
   final double _min;
 
   @override
   void paint(Canvas canvas, Size size) {
-    print('max: $_max, min: $_min: size: $size');
-
     final double widthNormalizer = size.width / (dataPoints.length - 1);
     final double heightNormalizer = size.height / (_max - _min);
 
-    print('wn: $widthNormalizer, hn: $heightNormalizer');
-
     final Path path = new Path();
+    final List<Offset> points = <Offset>[];
 
     for (int i = 0; i < dataPoints.length; i++) {
       double x = i * widthNormalizer;
       double y = size.height - (dataPoints[i] - _min) * heightNormalizer;
-      print('painting: ${dataPoints[i]} ($x, $y)');
+
+      if (_drawPoints) {
+        points.add(new Offset(x, y));
+      }
+
       if (i == 0) {
         path.moveTo(x, y);
       } else {
@@ -125,6 +161,14 @@ class _SparklinePainter extends CustomPainter {
     }
 
     canvas.drawPath(path, paint);
+
+    if (_drawPoints) {
+      Paint pointsPaint = new Paint()
+        ..strokeCap = StrokeCap.round
+        ..strokeWidth = pointSize ?? lineWidth
+        ..color = pointColor ?? lineColor;
+      canvas.drawPoints(ui.PointMode.points, points, pointsPaint);
+    }
   }
 
   @override
